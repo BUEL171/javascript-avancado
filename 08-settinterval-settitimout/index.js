65cedd67469695b442df3f5cb54edb9a8a4c3959
gabriel soares <soaresgabriel155@gmail.com>
2024-09-25 15:55:18 -0300

    aulas javascript avançado

---
+++ 08-settinterval-settitimout/index.js
@@ -0,0 +1,21 @@
+// setTimeout(() => {
+//     alert("ola mundo")
+// });
+
+// setTimeout(() => {
+//     console.log("console dentro do setTimout")
+// }, 4000);
+
+// setTimeout(() => {
+//     console.log("Executando a cada 2 segundos")
+// }, 2000);
+
+// console.log("console depois do setTimout")
+
+const idDoIntervalo = setInterval(() => {
+                        console.log("Executando a cada 2 segundos")
+                    }, 2000);
+
+console.log(idDoIntervalo)
+clearInterval(idDoIntervalo)
+clearTimeout()
