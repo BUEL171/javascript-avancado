040066c50f24b6f45713e13ab00b864427abc554
gabriel soares <soaresgabriel155@gmail.com>
2024-09-30 15:36:30 -0300

    aulas

---
+++ 10-promises/index.js
@@ -0,0 +1,28 @@
+/*
+        Pendente
+        Realizada Resolve
+        Recusado Reject
+        Estabelecida
+*/
+
+// new Promise((resolve, reject) => {
+    
+// })
+
+let ferverAgua = (chaleiraEstaNoFogo, fogaoEstaLigado) => {
+    return new Promise((resolve, reject) => {
+        if (chaleiraEstaNoFogo && fogaoEstaLigado) {
+            console.log('começando o processo de ferver água')
+            resolve()
+        }else {
+            console.log("é necessário ligar o fogão e colocar a chaleira no fogão para ferver a água")
+            reject()
+        }
+    })
+}
+
+
+let chaleiraEstaNoFogo = true
+let fogaoEstaLigado = true
+ferverAgua(chaleiraEstaNoFogo, fogaoEstaLigado)
+console.log("fazendo café")
